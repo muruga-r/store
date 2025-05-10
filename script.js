5ab8e060b1f3272d6b80d0b95a3bb2c835016938
muruga-r <enithamurugar@gmail.com>
2025-05-10 21:18:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function displayCart() {
   });
 }
 
-function orderNow() {
+/*function orderNow() {
   if (cart.length === 0) {
   alert("Cart is empty!");
   return;
@@ -25,10 +25,10 @@ function orderNow() {
   const mailtoLink = `mailto:govendhan4196@gmail.com?subject=New Order&body=Order Details:\n${orderDetails}\nTime: ${new Date().toLocaleString()}`;
   
   window.location.href = mailtoLink;
-  }
+  }*/
   
 
-/*function orderNow() {
+function orderNow() {
   if (cart.length === 0) {
     alert("Cart is empty!");
     return;
@@ -45,7 +45,7 @@ function orderNow() {
   const mailtoLink = `mailto:enithapriyadharshini@student.tce.edu?subject=New Order&body=${encodeURIComponent(mailBody)}`;
 
   window.location.href = mailtoLink;
-}*/
+}
 
 function clearCart() {
     cart = [];
